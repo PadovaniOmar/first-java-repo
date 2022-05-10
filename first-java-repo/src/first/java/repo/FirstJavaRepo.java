4cf5e640d6e3c6c4ee0bb786135baea0a077df7e
Padovani Omar Oscar <padovani.omar@osdbsesto>
2022-05-10 13:29:52 +0200

    add loop

--- first-java-repo/src/first/java/repo/FirstJavaRepo.java
+++ first-java-repo/src/first/java/repo/FirstJavaRepo.java
@@ -15,7 +15,9 @@ public class FirstJavaRepo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      for(int i=0;;i++){
+      System.out.print(i);
+      }
     }
     
 }
